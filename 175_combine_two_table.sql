d0cea8b0aaa22627becf75597fe0578c07455791
Yvie Hu <160700431+hy-yvieee@users.noreply.github.com>
2026-07-06 13:05:20 -0400

    Update SQL query to join Person and Address tables

--- 175_combine_two_table.sql
+++ 175_combine_two_table.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select email as Email
-from Person
-group by email
-having count(email) > 1;
+select p.firstName, p.lastName, a.city, a.state
+from Person p
+left join Address a on p.personId = a.personId;
